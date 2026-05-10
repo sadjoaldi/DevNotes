e1e2a9d5db42d62f3eb98d14790abc4b5ebef0c6
Sadjoaldi <alsannealdi@hotmail.com>
2026-05-10 05:00:57 +0200

    update: update server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,23 @@
+import type { Express } from "express";
 import express from "express";
 import morgan from "morgan";
-import noteRoutes from "./routes/noteRoutes";
+import bugReportRoutes from "./routes/bugReportRoutes";
 import tagRoutes from "./routes/tagRoutes";
+import technologyRoutes from "./routes/technologyRoutes";
 
-const app = express();
-const PORT = 3000;
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
 
-//middlewares
+// Middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 
-//Routes
-app.use("/api/v1/notes", noteRoutes);
+// Routes
+app.use("/api/v1/bug-reports", bugReportRoutes);
 app.use("/api/v1/tags", tagRoutes);
+app.use("/api/v1/technologies", technologyRoutes);
 
-//server
+// Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
